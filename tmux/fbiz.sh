15c1da9a0b31c35dda6339d597db11cf7c5757e7
Max Hung <info@maxhung.co.nz>
2016-06-06 12:57:30 +1200

    Update dir locations

--- tmux/fbiz.sh
+++ tmux/fbiz.sh
@@ -8,7 +8,7 @@ if [ $? != 0 ]
 then
   tmux -2 new-session -d -s $SESSION_NAME
   tmux rename-window -t $SESSION_NAME:1 'halfback'
-  tmux send-keys -t $SESSION_NAME 'cd ~/WORK/FBiz/halfback' Enter
+  tmux send-keys -t $SESSION_NAME 'cd ~/Work/fashionbiz/halfback' Enter
 fi
 
 SESSION_NAME=frontend
@@ -20,14 +20,14 @@ then
   tmux -2 new-session -d -s $SESSION_NAME
 
   tmux rename-window -t $SESSION_NAME:1 'COL'
-  tmux send-keys -t $SESSION_NAME:1 'cd ~/WORK/FBiz/biz-collection' Enter
+  tmux send-keys -t $SESSION_NAME:1 'cd ~/Work/fashionbiz/biz-collection' Enter
 
   tmux new-window -t $SESSION_NAME:2 -n 'CPR'
-  tmux send-keys -t $SESSION_NAME:2 'cd ~/WORK/FBiz/biz_corporates' Enter
+  tmux send-keys -t $SESSION_NAME:2 'cd ~/Work/fashionbiz/biz_corporates' Enter
 
   tmux new-window -t $SESSION_NAME:3 -n 'SMK'
-  tmux send-keys -t $SESSION_NAME:3 'cd ~/WORK/FBiz/syzmik' Enter
+  tmux send-keys -t $SESSION_NAME:3 'cd ~/Work/fashionbiz/syzmik' Enter
 
   tmux new-window -t $SESSION_NAME:4 -n 'FFC'
-  tmux send-keys -t $SESSION_NAME:4 'cd ~/WORK/FBiz/fbz-frontend-core' Enter
+  tmux send-keys -t $SESSION_NAME:4 'cd ~/Work/fashionbiz/fbz-frontend-core' Enter
 fi
