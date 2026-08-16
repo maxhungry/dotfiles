58fc9a427d04ae56cb83982a21fe12d7cf53d31d
Max Hung <info@maxhung.co.nz>
2026-08-16 21:22:24 +1200

    Add neotest

--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -2,5 +2,41 @@ return {
   {
     "nvim-neotest/neotest",
     commit = "52fca6717ef972113ddd6ca223e30ad0abb2800c",
+    optional = true,
+    dependencies = {
+      "olimorris/neotest-rspec",
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      -- "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      adapters = {
+        ["neotest-rspec"] = {
+          rspec_cmd = function()
+            return vim.tbl_flatten({
+              "docker",
+              "compose",
+              "exec",
+              "-i",
+              "-w",
+              "/app",
+              "-e",
+              "RAILS_ENV=test",
+              "web",
+              "bin/rspec",
+            })
+          end,
+
+          transform_spec_path = function(path)
+            local prefix = require("neotest-rspec").root(path)
+            return string.sub(path, string.len(prefix) + 2, -1)
+          end,
+
+          results_path = "tmp/rspec.output",
+          formatter = "json",
+        },
+      },
+    },
   },
 }
